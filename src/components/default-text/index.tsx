6fd27674387396c50c242669987ab8b2bfef3701
conwu7 <clintononwu@yahoo.com>
2023-12-31 10:04:08 -0600

    Keep Font splashscreen helpers

--- src/components/default-text/index.tsx
+++ src/components/default-text/index.tsx
@@ -1,4 +1,5 @@
 import {
+  useFonts,
   RedHatDisplay_300Light,
   RedHatDisplay_400Regular,
   RedHatDisplay_500Medium,
@@ -6,12 +7,14 @@ import {
   RedHatDisplay_700Bold,
   RedHatDisplay_800ExtraBold,
   RedHatDisplay_900Black,
-  useFonts,
 } from '@expo-google-fonts/red-hat-display';
 import {Bungee_400Regular} from '@expo-google-fonts/bungee';
 import {Text} from 'react-native';
 import * as React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import {DefaultFonts} from '../../shared/enums';
+
+SplashScreen.preventAutoHideAsync();
 
 interface DefaultTextProps {
   weight?: 300 | 400 | 500 | 600 | 700 | 800 | 900 | 1000;
@@ -50,6 +53,8 @@ export default function DefaultText(props: DefaultTextProps) {
   if (!fontsLoaded) {
     return null;
   }
+
+  SplashScreen.hideAsync();
 
   return (
     <Text {...props} style={[style, ...customStyles]}>
